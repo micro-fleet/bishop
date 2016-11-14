bceda29516c55f88782c4730307e0ac605d125bd
Andrey <vkfont@gmail.com>
2016-11-14 18:17:24 +0300

    test engine

---
+++ test/core.js
@@ -0,0 +1,11 @@
+const { test } = require('ava')
+
+const bishop = require(`${process.env.PWD}/src`)()
+
+test.todo('add pattern')
+
+test.todo('use plugin')
+
+test.todo('act nonexisting pattern')
+
+test.todo('act existing pattern')
