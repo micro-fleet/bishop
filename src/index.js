9ec352e07ef870423b1593ff0fe6343c017e562e
Andrey <vkfont@gmail.com>
2016-11-16 12:39:16 +0300

    additional pattern flags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const patrun = require('patrun')
 const ld = require('lodash')
+const { EventEmitter } = require('events')
 const { objectify } = require('./utils')
 const Promise = require('bluebird')
 
@@ -8,13 +9,15 @@ const defaultConfig = {
   timeout: 500
 }
 
-module.exports = (_config = {}) => {
+const Bishop = (_config = {}) => {
+
   const pm = patrun({ gex:true })
+  const pmLocal = patrun({ gex:true })
   const config = ld.assign({}, defaultConfig, _config)
 
   return {
 
-    log: console,
+    events: new EventEmitter(),
 
     routes: {},
 
@@ -25,41 +28,58 @@ module.exports = (_config = {}) => {
     // .add(route, function) // execute local payload
     // .add(route, 'transportname') // execute payload using transport
     add(_pattern, handler) {
-      const $type = ld.isFunction(handler) ? 'local' : handler
+      const type = ld.isFunction(handler) ? 'local' : handler
       const pattern = objectify(_pattern)
+      const payload = { type, handler }
 
-      pm.add(pattern, {
-        type: $type,
-        handler: handler
-      })
+      pm.add(pattern, payload)
+      if (type === 'local') {
+        pmLocal.add(pattern, payload)
+      }
     },
 
     remove(_pattern) {
       pm.remove(objectify(_pattern))
+      pmLocal.remove(objectify(_pattern))
     },
 
-    // expect options as last parameter
-    // $timeout - redefine global request timeout
-    // 2do: $type - search only in specified transports (default: 'local')
-    // 2do: $nowait - resolve then message is sent, dont wait for answer {not implemented}
+    // $timeout - redefine global request timeout for network requests
+    // $local - search only in local patterns, skip remote transporting
+    // $nowait - resolve immediately (in case of local patters), or then message is sent (in case of transports)
     async act(_pattern, payload = {}) {
 
       if (!_pattern) { throw new Error('pattern not specified') }
       const pattern = ld.assign({}, objectify(_pattern), payload)
-      const matchResult = pm.find(pattern)
+      const matchResult = (pattern.$local ? pmLocal : pm).find(pattern)
       if (!matchResult) {
         throw new Error(`pattern not found: ${JSON.stringify(pattern)}`)
       }
 
       const { type, handler } = matchResult
-      const executor = type === 'local' ? handler : this.remote[type].act
+      const isLocalPattern = type === 'local'
+
+      const method = isLocalPattern ? handler : this.remote[type].act // wrap with network call
+
+      // in case of local pattern - resolve immediately and emit error on fail
+      // in case of transports - they should respect $nowait flag and emit errors manually
+      // all errors durning execution progress should be emitted as async using `this.events` eventemitter
+      const executor = isLocalPattern && pattern.$nowait ? (...input) => {
+        method(...input).catch(err => this.events.emit('error', err))
+        return Promise.resolve()
+      }: method
 
       // setup ttl and execute payload
       const timeout = pattern.$timeout || config.timeout
-      return Promise.resolve(executor(pattern)).timeout(timeout)
-      .catch(Promise.TimeoutError, () => {
-        throw new Error(`pattern timeout after ${timeout}ms: ${JSON.stringify(pattern)}`)
-      })
+
+      if (!timeout) {
+        return executor(pattern)
+      }
+      return Promise
+        .resolve(executor(pattern))
+        .timeout(timeout)
+        .catch(Promise.TimeoutError, () => {
+          throw new Error(`pattern timeout after ${timeout}ms: ${JSON.stringify(pattern)}`)
+        })
     },
 
     // load plugin, module etc
@@ -90,3 +110,5 @@ module.exports = (_config = {}) => {
 
   }
 }
+
+module.exports = Bishop
