e1cf265c5a0fa671f9ae94aa615e25b3131da55f
Anfey <vkfont@yandex.ru>
2017-03-07 12:21:54 +0300

    Fix some stuff

--- src/utils.js
+++ src/utils.js
@@ -60,7 +60,7 @@ module.exports = {
       if (ld.isPlainObject(value)) {
         return `${key}:{${ld.keys(value).join(',')}}`
       }
-      return `${key}:${(value || '*').toString()}`
+      return value ? `${key}:${value.toString()}` : key
     }).join(', ')
   }
 }
